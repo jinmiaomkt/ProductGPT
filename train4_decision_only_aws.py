9c4721bb1ff993ade7a2dc10d52d1de35c7de07b
jinmiaomkt <jxm190071@utdallas.edu>
2025-05-20 11:24:19 -0500

    change

--- train4_decision_only_aws.py
+++ train4_decision_only_aws.py
@@ -190,7 +190,7 @@ def get_dataloaders(config):
     train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
     val_loader   = DataLoader(val_dataset,   batch_size=config['batch_size'], shuffle=False)
     test_loader  = DataLoader(test_dataset,  batch_size=config['batch_size'], shuffle=False)
-    return train_loader, val_loader, test_loader
+    return train_loader, val_loader, test_loader, tokenizer_tgt
 
 ##############################################################################
 # build_model
