1a329beb10bf7b672f5459be68594d84e937a1fb
jinmiaomkt <jxm190071@utdallas.edu>
2026-04-03 11:27:19 -0500

    change

--- commmand_collection.sh
+++ commmand_collection.sh
@@ -532,3 +532,14 @@ python3 unified_model_eval.py \
   --output-dir /home/ec2-user/eval_unified_fold0 \
   --s3 s3://productgptbucket/evals/unified_compare_$(date +%F_%H%M%S)/ \
   --save-preds
+
+python3 unified_model_eval.py \
+  --config    model_specs_example.json \
+  --data      /home/ec2-user/data/clean_list_int_wide4_simple6.json \
+  --labels    /home/ec2-user/data/clean_list_int_wide4_simple6.json \
+  --uids-val  s3://productgptbucket/ProductGPT/CV/exp_001/train/fold0/uids_val.txt \
+  --uids-test s3://productgptbucket/ProductGPT/CV/exp_001/train/fold0/uids_test.txt \
+  --fold-id   0 \
+  --output-dir /tmp/unified_eval_fold0 \
+  --s3        "s3://productgptbucket/evals/unified_fold0_$(date +%F_%H%M%S)/" \
+  --compare-on test
